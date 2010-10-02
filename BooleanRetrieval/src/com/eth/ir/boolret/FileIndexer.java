8e79e71ef1227edf34022043a325fb543a77c3f3
mehta.anshuman <mehta.anshuman@gmail.com>
2010-10-02 01:36:36 +0000

    some more modifications. the part related to querying is left

--- BooleanRetrieval/src/com/eth/ir/boolret/FileIndexer.java
+++ BooleanRetrieval/src/com/eth/ir/boolret/FileIndexer.java
@@ -37,8 +37,9 @@ public class FileIndexer {
         return new PostingList();
     }
 
-    public void addToDictionary(Integer docId, String key) {
-        if (!index.containsKey(key)) {
+    public void addToDictionary(String docId, String key) {
+//        System.out.println("Adding---" + key);
+        if (!index.containsKey(key.trim())) {
             //create a new PostingList pl
             //create a new PostingListNode pln
             // set docid to pln
@@ -49,7 +50,7 @@ public class FileIndexer {
             PostingList pl = new PostingList();
             PostingListNode pln = new PostingListNode(docId, new Integer(1));
             pl.getPostingList().add(pln);
-            index.put(key, pl);
+            index.put(key.trim(), pl);
         } else {
             // fetch the postinglist pl
             // retrieve the postinglistnode pln for given docid
@@ -57,17 +58,17 @@ public class FileIndexer {
             // System.out.println(" key = " + key + " docId " + docId);
             PostingList pl = index.get(key);
             // this terms is in the same docId. increment it for this docId only
-            if (docId == pl.getPostingList().getLast().getDocId()) {
+            if (docId.equals(pl.getPostingList().getLast().getDocId())) {
                 PostingListNode pln = pl.getPostingList().getLast();
                 pln.setFrequencyInDoc(pln.getFrequencyInDoc() + 1);
-                index.put(key, pl);
+                index.put(key.trim(), pl);
             } // this term is for a new docId
             // create a new pln and append it
             // initiate the frequency for this docId
             else {
                 PostingListNode pln = new PostingListNode(docId, new Integer(1));
                 pl.getPostingList().add(pln);
-                index.put(key, pl);
+                index.put(key.trim(), pl);
             }
 
 
@@ -75,13 +76,21 @@ public class FileIndexer {
 
     }
 
-    public void tokenizeFile(Integer docId, File file) {
+    public void tokenizeFile(String docId, File file) {
+        System.out.println("Tokeninzing --- " + docId);
         FileReader reader = null;
         BufferedReader in = null;
         try {
             reader = new FileReader(file);
             in = new BufferedReader(reader);
             StreamTokenizer st = new StreamTokenizer(in);
+
+            st.ordinaryChars(33,47);
+            st.ordinaryChars(58, 64);
+//            st.ordinaryChar('\'');
+//            st.ordinaryChar('"');
+//            st.ordinaryChar('/');
+//            st.ordinaryChar(';');
 
             int next = st.nextToken();
 
@@ -91,7 +100,7 @@ public class FileIndexer {
                     case StreamTokenizer.TT_NUMBER:
 //                        System.out.println("nextn = " + st.nval);
                         Double val = st.nval;
-                        addToDictionary(docId, val.toString());
+                        addToDictionary(docId, val.toString().trim());
 
                         break;
 
@@ -99,16 +108,18 @@ public class FileIndexer {
                         // StreamTokenizer sometimes reads in words with ','s together.
                         // this is to split it up exactly as we want it.
                         String stoken = st.sval;
+                        if(stoken.contains("border".toUpperCase()))
+                            System.out.println("FOUND1------------");
                         if (stoken.contains(",")) {
                             String xtokens[] = stoken.split(",");
                             for (String xtoken : xtokens) {
                                 if (!xtoken.trim().equals("")) {
-                                    addToDictionary(docId, xtoken);
+                                    addToDictionary(docId, xtoken.trim());
                                 }
                             }
                         } else {
                             // It is without a ',' and works for us
-                            addToDictionary(docId, stoken);
+                            addToDictionary(docId, stoken.trim());
                         }
                         break;
                     default:
@@ -135,12 +146,12 @@ public class FileIndexer {
     public void fetchFilesInDirectory(String dir) {
         File directory = new File(dir);
         File[] files = directory.listFiles();
-        Integer docId = 0;
+     //   Integer docId = 0;
         for (File file : files) {
             //System.out.println("Indexing " + file.getName() + " docId " + docId);
-            docIndex.put(docId, file.getName());
-            tokenizeFile(docId, file);
-            docId++;
+       //     docIndex.put(docId, file.getName());
+            tokenizeFile(file.getName(), file);
+         //   docId++;
         }
     }
 
@@ -149,7 +160,7 @@ public class FileIndexer {
         for (Map.Entry<String, PostingList> entry : index.entrySet()) {
             String key = entry.getKey();
             PostingList value = entry.getValue();
-            System.out.println("key = " + key + " PostingListSize =" + value.getPostingList().size());
+            System.out.println("key = " + key + " PostingList =" + value.getPostingList().toString());
         }
     }
 
@@ -182,13 +193,13 @@ public class FileIndexer {
 
         String dir = "F://ETH//Projects//InformationRetrieval//BooleanRetrieval//Docs";
         String indexFile = "F://ETH//Projects//InformationRetrieval//BooleanRetrieval//Docs//index";
-        String dirIndexFile = "F://ETH//Projects//InformationRetrieval//BooleanRetrieval//Docs//dirIndex";
+       // String dirIndexFile = "F://ETH//Projects//InformationRetrieval//BooleanRetrieval//Docs//dirIndex";
         FileIndexer tkz = new FileIndexer();
         tkz.fetchFilesInDirectory(dir);
        // System.out.println("what = " + tkz.index.size());
        tkz.printIndex();
        tkz.serializeToFile(indexFile,1);
-       tkz.serializeToFile(dirIndexFile, 2);
+     //  tkz.serializeToFile(dirIndexFile, 2);
 
 
     }
