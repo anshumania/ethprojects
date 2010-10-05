5080d1b045027eb5de0f27fcd7e6d96b95f9c335
mehta.anshuman <mehta.anshuman@gmail.com>
2010-10-05 20:05:55 +0000

    Added Stats for AND , NOT , OR

--- BooleanRetrieval/src/com/eth/ir/boolret/QueryParser.java
+++ BooleanRetrieval/src/com/eth/ir/boolret/QueryParser.java
@@ -13,6 +13,7 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.logging.Level;
@@ -39,6 +40,7 @@ public class QueryParser {
         TreeMap<String,PostingList> solutionSpace = new TreeMap<String,PostingList>();
         HashSet<String> result = new HashSet<String>();
         // PostingList -> LinkedList of PostingListNodes [docId, freq]
+        long currentTime = System.nanoTime();
         for(String term : terms)
         {
             PostingList pl = index.get(term);
@@ -47,13 +49,17 @@ public class QueryParser {
                 result.add(pln.getDocId());
             }
         }
+        long executionTime = System.nanoTime() - currentTime;
         //System.out.println("Result = " + result);
 
         for(String id : result) {
             System.out.println(id);
         }
+
+        System.out.println("The Execution Time in nanoseconds " + executionTime);
     }
 
+    
     public void doNOTQuery(Query query)
     {
         String terms[] = query.getTerms();
@@ -61,6 +67,7 @@ public class QueryParser {
         //HashMap<String,Set<String>> result = new HashMap<String,Set<String>>();
         List<Set<String>> result = new ArrayList<Set<String>>();
         // PostingList -> LinkedList of PostingListNodes [ docId, freq]
+        long currentTime = System.nanoTime();
         for(String term : terms)
         {
             //System.out.println("termsss=" + term);
@@ -74,6 +81,8 @@ public class QueryParser {
             result.add(docSet);
 
         }
+
+
 
         //System.out.println(" result = " + result);
         Set<String> partialSet = new HashSet<String>();
@@ -89,11 +98,106 @@ public class QueryParser {
            else
                partialSet.removeAll(iterator);
         }
+        long executionTime = System.nanoTime() - currentTime;
 
         //System.out.println("Result = " + partialSet);
         for(String id : partialSet) {
             System.out.println(id);
         }
+        System.out.println("The Execution Time in nanoseconds = " + executionTime);
+    }
+
+    public void collectStatistics(Query query)
+    {
+        
+        String terms[] = query.getTerms();
+        TreeMap<String,PostingList> solutionSpace = new TreeMap<String,PostingList>();
+        TreeMap<Integer,ArrayList<Set<String>>> result = new TreeMap<Integer,ArrayList<Set<String>>>();
+
+        // PostingList -> LinkedList of PostingListNodes [ docId, freq]
+
+
+        for(String term : terms)
+        {
+            PostingList pl = index.get(term);
+            solutionSpace.put(term, pl);
+            HashSet<String> docSet = new HashSet<String>();
+            for(PostingListNode pln : pl.getPostingList()) {
+                docSet.add(pln.getDocId());
+            }
+            if(result.containsKey(pl.getPostingList().size()))
+            {
+                ArrayList<Set<String>> temp = result.get(pl.getPostingList().size());
+                temp.add(docSet);
+                result.put(pl.getPostingList().size(), temp);
+            }
+            else
+            {
+                ArrayList<Set<String>> temp = new ArrayList<Set<String>>();
+                temp.add(docSet);
+                result.put(pl.getPostingList().size(), temp);
+            }
+        }
+
+     //   System.out.println("result=" + result);
+
+        long currentTime = System.nanoTime();
+
+        ArrayList<Set<String>> partialSet = new ArrayList<Set<String>>();
+        Set<String> firstSet = new HashSet<String>();
+        boolean first = true;
+        for(Map.Entry<Integer,ArrayList<Set<String>>> iterator : result.entrySet())
+        {
+
+
+               for(Set<String> loopSet : iterator.getValue())
+               {
+                   if(first)
+                   {
+                       firstSet = loopSet;
+                       first = false;
+                   }
+                    else
+                    firstSet.retainAll(loopSet);
+               }
+                
+        }
+        long executionTime = System.nanoTime() - currentTime;
+        System.out.println("Execution Time fastest in nanoseconds = " + executionTime);
+        //System.out.println("Result = " + partialSet);
+
+
+
+
+       NavigableMap<Integer,ArrayList<Set<String>>> reverse = result.descendingMap();
+       // System.out.println("reverse=" + reverse);
+        currentTime = System.nanoTime();
+        first = true;
+        for(Map.Entry<Integer,ArrayList<Set<String>>> iterator : reverse.entrySet())
+        {
+
+
+               for(Set<String> loopSet : iterator.getValue())
+               {
+                   if(first)
+                   {
+                       firstSet = loopSet;
+                       first = false;
+                   }
+                    else
+                    firstSet.retainAll(loopSet);
+               }
+
+        }
+       
+       executionTime = System.nanoTime() - currentTime;
+
+       System.out.println("Execution Time slowest in nanoseconds = " + executionTime);
+
+     
+ 
+        
+
     }
 
     public void doANDQuery(Query query)
@@ -114,6 +218,8 @@ public class QueryParser {
             result.put(term.trim(), docSet);
         }
 
+
+
         Set<String> partialSet = new HashSet<String>();
         boolean first = true;
         for(Map.Entry<String,Set<String>> iterator : result.entrySet())
@@ -125,6 +231,8 @@ public class QueryParser {
                partialSet.retainAll(iterator.getValue());
             }
         }
+
+
 
         //System.out.println("Result = " + partialSet);
         for(String id : partialSet) {
@@ -189,6 +297,7 @@ public class QueryParser {
 
         if(operator.equalsIgnoreCase(Query.AndOperator)) {
             doANDQuery(q);
+            collectStatistics(q);
 
             /*
             //merge each pair of terms with the result of the previous merge
@@ -257,9 +366,10 @@ public class QueryParser {
 
     public static void main(String args[]) {
         QueryParser x = new QueryParser();
-        //String indexFile = "F://ETH//Projects//InformationRetrieval//ethprojects//BooleanRetrieval//Docs//index";
-        String indexFile = "C://Users//ghff//Documents//ETH//Fall 2010//Information Retrieval//Dataset//index";
-        String queryDir = "C://Users//ghff//Documents//ETH//Fall 2010//Information Retrieval//Dataset//Queries";
+        String indexFile = "F://ETH//Projects//InformationRetrieval//ethprojects//BooleanRetrieval//index";
+        //String indexFile = "C://Users//ghff//Documents//ETH//Fall 2010//Information Retrieval//Dataset//index";
+        String queryDir = "F://ETH//Projects//InformationRetrieval//ethprojects//BooleanRetrieval//Queries";
+        //String queryDir = "C://Users//ghff//Documents//ETH//Fall 2010//Information Retrieval//Dataset//Queries";
 
         //Load index from file
         x.readIndex(indexFile);
