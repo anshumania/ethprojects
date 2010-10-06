a834b2d741701f736ce66ac8c47dbc3a19d53d1f
tim.church <tim.church@e4925ead-2125-3ea3-71bd-096e68c73fe8>
2010-10-06 14:21:52 +0000

    Renamed SimpleClient to ConsoleClient Refactored Middleware to use Threads (moved most logic to MiddlewareThread)

--- trunk/EnterpriseApplicationRetrieval/src/eai/Middleware.java
+++ trunk/EnterpriseApplicationRetrieval/src/eai/Middleware.java
@@ -1,26 +1,12 @@
 package eai;
 
-import eai.db.Address;
-import eai.db.Country;
-import eai.db.Customer;
-import eai.db.DAOInterface;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Level;
@@ -31,21 +17,17 @@ import java.util.logging.Logger;
  * @author Tim Church
  */
 public class Middleware {
-    //final String dbIds[] = {"Z", "B"};
-    //final String databases[] = {"eai_zurich", "eai_bern"};
+
     private static final Map<String, String> databases =
-        Collections.unmodifiableMap(new HashMap<String, String>() {{
+            Collections.unmodifiableMap(new HashMap<String, String>() {
+
+        {
             put("Z", "eai_zurich");
             put("B", "eai_bern");
-        }});
-
+        }
+    });
     HashMap<String, Connection> connections;
-    Boolean connectionsClosed = false;  //checks whether close() method has been called yet.
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    PrintWriter socketOut = null;
-    BufferedReader socketIn = null;
-
 
     public Middleware() {
         initMiddleware();
@@ -65,20 +47,20 @@ public class Middleware {
             Class.forName("org.postgresql.Driver").newInstance();
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println("Unable to connect to PostgreSQL driver");
+            System.exit(-1);
         } catch (InstantiationException ex) {
             Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println("Unable to connect to PostgreSQL driver");
+            System.exit(-1);
         } catch (IllegalAccessException ex) {
             Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println("Unable to connect to PostgreSQL driver");
+            System.exit(-1);
         }
 
         //create one JDBC connection to each database
-        for(Entry<String,String> dbEntry : databases.entrySet()) {
+        for (Entry<String, String> dbEntry : databases.entrySet()) {
             try {
                 //create a connection
-                Connection con = DriverManager.getConnection("jdbc:postgresql:" + dbEntry.getValue() +  "?user=" + dbUsername + "&password=" + dbPassword);
+                Connection con = DriverManager.getConnection("jdbc:postgresql:" + dbEntry.getValue() + "?user=" + dbUsername + "&password=" + dbPassword);
                 con.setAutoCommit(true);
                 connections.put(dbEntry.getKey(), con);
             } catch (SQLException ex) {
@@ -88,264 +70,42 @@ public class Middleware {
         }
     }
 
-    public void runServer() {
+    public void runServer() throws IOException {
         //start server socket
+        ServerSocket serverSocket = null;
         try {
-            this.serverSocket = new ServerSocket(4444);
-            this.clientSocket = this.serverSocket.accept();
-            this.socketOut = new PrintWriter(this.clientSocket.getOutputStream(), true);
-            this.socketIn = new BufferedReader( new InputStreamReader( this.clientSocket.getInputStream() ));
-            System.out.println("Middleware server is running");
-
-            String request;
-            ArrayList<String> response = new ArrayList<String>();
-            while ((request = this.socketIn.readLine()) != null) {
-                //this.socketOut.println(request); //echo request for now
-                response = executeRequest(request);
-                this.socketOut.println(response.size());
-                for(String responseLine : response) {
-                    this.socketOut.println(responseLine);
-                }
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(-1);
         }
+        System.out.println("Middleware server started");
+
+        boolean listening = true;
+        while (listening) {
+            new MiddlewareThread(serverSocket.accept(), this.connections).start();
+        }
+
+        serverSocket.close();
+        this.closeJDBCConnections();
     }
 
-    //Clean up method that should always be called after using a Middleware
-    public void closeConnections() {
-        //close all JDBC connections
-        for(Connection con : this.connections.values()) {
+    //Clean up method - closes all JDBC connections
+    public void closeJDBCConnections() {
+        for (Connection con : this.connections.values()) {
             try {
                 con.close();
             } catch (SQLException ex) {
                 Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
-        //close sockets
-        try {
-            this.socketIn.close();
-            this.socketOut.close();
-            this.clientSocket.close();
-            this.serverSocket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        this.connectionsClosed = true;
-    }
-
-    /**
-     * Tries to make sure that all db connections are closed during cleanup.
-     * @throws Throwable
-     */
-    @Override
-    protected void finalize() throws Throwable {
-        if(!this.connectionsClosed) {
-            //log warning and close all connections.
-            Logger.getLogger(Middleware.class.getName()).log(Level.WARNING, "closeConnections was never called.  Closing connections now in cleanup.");
-            this.closeConnections();
-        }
-        super.finalize();
-    }
-
-    private ArrayList<String> executeRequest(String request) {
-        ArrayList<String> response = new ArrayList<String>();
-        
-        //parse request
-        String params[] = request.split("[|]");
-
-        //TODO - validate input request
-        String location = params[0]; //'Z' or 'B' (or 'A')
-        String operation = params[1];//'C', 'V', 'U', or 'D'
-        String table = params[2];    //'C' or 'A'
-
-        //trim off the paramaters we have already used
-        String functionParams[] = new String[params.length - 3];
-        System.arraycopy(params,3,functionParams,0,params.length - 3);
-
-        try {
-            if(operation.equals("C")) {
-                //'C' == CREATE (i.e. INSERT)
-                int numCreatedRows = doCreateQuery(location, table, functionParams);
-                response.add(Integer.toString(numCreatedRows) + " row(s) created");
-            } else if(operation.equals("V")) {
-                //'V' == VIEW (i.e. SELECT)
-                //return doViewQuery(location, table);
-                return doViewQuery(table); //TEMP - only support Customers table for view.  params[2] == is actual location value
-            } else if(operation.equals("U")) {
-                //'U' == UPDATE
-                int numUpdatedRows = doUpdateQuery(location, table, functionParams);
-                response.add(Integer.toString(numUpdatedRows) + " row(s) updated");
-            } else if(operation.equals("D")) {
-                //'D' == DELETE
-                int numDeletedRows = doDeleteQuery(location, table, Integer.valueOf(functionParams[0]).intValue());
-                response.add(Integer.toString(numDeletedRows) + " row(s) deleted");
-            }/* TODO - else throw error */
-        } catch(SQLException ex) {
-            Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
-            String errors[] = ex.getMessage().split("[\n]");
-            for(String error : errors) {
-                response.add(error);
-            }
-        }
-
-        return response;
-    }
-
-    private int doCreateQuery(String location, String table, String[] paramList) throws SQLException {
-        PreparedStatement statement = null;
-        if(table.equalsIgnoreCase("C")) {
-            //'C' == CUSTOMER TABLE
-            statement = Customer.getInsertStatement(this.connections.get(location));
-            statement.setString(1, paramList[0]);
-            statement.setString(2, paramList[1]);
-            statement.setString(3, paramList[2]);
-            statement.setString(4, paramList[3]);
-            statement.setString(5, paramList[4]);
-        } else if(table.equalsIgnoreCase("A")) {
-            //'A' == ADDRESS TABLE
-            //First need to check Country already exists in Country table
-            int countryId = findCountryId(location, paramList[4]);
-            statement = Address.getInsertStatement(this.connections.get(location));
-            statement.setInt(1, Integer.valueOf(paramList[0]).intValue());
-            statement.setString(2, paramList[1]);
-            statement.setString(3, paramList[2]);
-            statement.setString(4, paramList[3]);
-            statement.setInt(5, countryId);
-        }
-
-        return statement.executeUpdate();
-    }
-
-    
-    private ArrayList<String> doViewQuery(String location) throws SQLException {
-        ArrayList<String> response = new ArrayList<String>();
-        PreparedStatement statements[] = null;
-        if(location.equalsIgnoreCase("A")) {
-            statements = new PreparedStatement[2];
-        } else {
-            statements = new PreparedStatement[1];
-        }
-
-        //if(table.equalsIgnoreCase("C")) {
-            //'C' == CUSTOMER TABLE
-            if(location.equalsIgnoreCase("A")) {
-                //'A' == ALL (i.e. BOTH ZURICH AND BERN)
-                statements[0] = Customer.getViewStatement(this.connections.get("Z"));
-                statements[1] = Customer.getViewStatement(this.connections.get("B"));
-            } else {
-                statements[0] = Customer.getViewStatement(this.connections.get(location));
-            }
-        /*} else if(table.equalsIgnoreCase("A")) {
-            //'A' == ADDRESS TABLE
-            if(location.equalsIgnoreCase("A")) {
-                //'A' == ALL (i.e. BOTH ZURICH AND BERN)
-                statements[0] = Address.getViewStatement(this.connections.get('Z'));
-                statements[1] = Address.getViewStatement(this.connections.get('B'));
-            } else {
-                statements[0] = Address.getViewStatement(this.connections.get(location));
-            }
-        }*/
-
-        final String databases[] = {"ZURICH", "BERN"};
-        int count = 0;
-        for(PreparedStatement statement : statements) {
-            response.add("FROM " + databases[count] + ":");
-            ResultSet rs =  statement.executeQuery();
-            while (rs.next()) {
-                //if(table.equalsIgnoreCase("C")) {
-                    Customer customer = new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getDate(7));
-                    response.add(customer.toString());
-                /*} else if(table.equalsIgnoreCase("A")) {
-                    //'A' == ADDRESS TABLE
-                    Address address = new Address(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
-                    response += address.toString();
-                }*/
-            }
-            rs.close();
-            count++;
-        }
-
-        return response;
-    }
-
-    private int doUpdateQuery(String location, String table, String[] paramList) throws SQLException {
-        PreparedStatement statement = null;
-        if(table.equalsIgnoreCase("C")) {
-            //'C' == CUSTOMER TABLE
-            statement = Customer.getUpdateStatement(this.connections.get(location));
-            statement.setString(1, paramList[1]);
-            statement.setString(2, paramList[2]);
-            statement.setString(3, paramList[3]);
-            statement.setString(4, paramList[4]);
-            statement.setString(5, paramList[5]);
-            statement.setInt(6, Integer.valueOf(paramList[0]).intValue());
-        } else if(table.equalsIgnoreCase("A")) {
-            //'A' == ADDRESS TABLE
-            //First need to check Country already exists in Country table
-            int countryId = findCountryId(location, paramList[4]);
-
-            statement = Address.getUpdateStatement(this.connections.get(location));
-            statement.setInt(1, Integer.valueOf(paramList[1]).intValue());
-            statement.setString(2, paramList[2]);
-            statement.setString(3, paramList[3]);
-            statement.setInt(4, countryId);
-            statement.setString(5, paramList[0]);
-        }
-
-        return statement.executeUpdate();
-    }
-
-    private int doDeleteQuery(String location, String table, int id) throws SQLException {
-        PreparedStatement statement = null;
-        if(table.equalsIgnoreCase("C")) {
-            //'C' == CUSTOMER TABLE
-             statement = Customer.getDeleteStatement(this.connections.get(location));
-             statement.setInt(1, id);
-        } else if(table.equalsIgnoreCase("A")) {
-            //'A' == ADDRESS TABLE
-            statement = Address.getDeleteStatement(this.connections.get(location));
-            statement.setInt(1, id);
-        }
-        return statement.executeUpdate();
-    }
-
-    public int findCountryId(String location, String countryName) throws SQLException {
-        int countryId = 0;
-        PreparedStatement statement = Country.getViewStatement(this.connections.get(location));
-        statement.setString(1, countryName);
-        ResultSet rs = statement.executeQuery();
-        if(rs.next()) {
-            countryId = rs.getInt("country_id");
-        } else {
-            //this is a new country, insert it
-            statement = Country.getInsertStatement(this.connections.get(location));
-            statement.setString(1,countryName);
-            int numRowsInserted = statement.executeUpdate();
-            if(numRowsInserted == 1) {
-                statement = Country.getViewStatement(this.connections.get(location));
-                statement.setString(1, countryName);
-                rs = statement.executeQuery();
-                rs.next();
-                countryId = rs.getInt("country_id");
-            }
-        }
-        return countryId;
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Middleware mw = new Middleware();
-        try {
-            mw.runServer();
-        } catch(Exception ex) {
-            Logger.getLogger(Middleware.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            mw.closeConnections();
-        }
+        mw.runServer();
     }
 }
