cc639e5951679bf8762e24cc7bea735235d22b06
mehta.anshuman@gmail.com <mehta.anshuman@gmail.com@e4925ead-2125-3ea3-71bd-096e68c73fe8>
2010-11-21 01:55:48 +0000

    minor updates

--- trunk/BooleanRetrieval/src/com/eth/ir/spamfiltering/naiveBayes/NaiveBayes.java
+++ trunk/BooleanRetrieval/src/com/eth/ir/spamfiltering/naiveBayes/NaiveBayes.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.eth.ir.spamfiltering.naiveBayes;
 
 import com.eth.ir.spamfiltering.SpamBundle;
@@ -21,15 +20,18 @@ import java.util.logging.Logger;
 public class NaiveBayes {
 
     NaiveBayesHelper nbHelper;
-    public enum sClass { SPAM, NOTSPAM };
-    double prior[];
-    Map<String,Double> conditionalProbabilityMap;
 
-    NaiveBayes()
-    {
+    public enum sClass {
+
+        SPAM, NOTSPAM
+    };
+    double prior[];
+    Map<String, Double> conditionalProbabilityMap;
+
+    NaiveBayes() {
         nbHelper = new NaiveBayesHelper();
         prior = new double[sClass.values().length];
-        conditionalProbabilityMap = new HashMap<String,Double>();
+        conditionalProbabilityMap = new HashMap<String, Double>();
     }
 
     public void trainMultinomialNB(String skipDir) //Classification C, DocumentSet D)
@@ -45,56 +47,51 @@ public class NaiveBayes {
 //                for each t<-V
 //                        do condprob[t][c]<- (Tct + 1) / sigmat* (Tct* + 1)
 //        return V, prior, condprob
-        
-        nbHelper.initializeNBHelper(SpamBundle.class.getResource(SpamBundle.DOC_CORPUS_DIR).getFile(),skipDir);
-        Map<String,Map<sClass,Integer>> vocabulary = nbHelper.extractVocabulary();
-        System.out.println("vocabulary="+ vocabulary.size());
-        int numDocsTrainingSet  = nbHelper.countDocs();
-        System.out.println("numDocsTrainingSet="+numDocsTrainingSet);
-        int i=0;
+
+        nbHelper.initializeNBHelper(SpamBundle.class.getResource(SpamBundle.DOC_CORPUS_DIR).getFile(), skipDir);
+        Map<String, Map<sClass, Integer>> vocabulary = nbHelper.extractVocabulary();
+        System.out.println("vocabulary=" + vocabulary.size());
+        int numDocsTrainingSet = nbHelper.countDocs();
+        System.out.println("numDocsTrainingSet=" + numDocsTrainingSet);
+        int i = 0;
 
         //double conditionalProbability[][] = new double[vocabulary.size()][sClass.values().length];
 
 
 
-        for(sClass sclass : sClass.values())
-        {
-            
-            int numOfDocsInClass = nbHelper.countDocsInClass(sclass);
-            System.out.println("numOfDocsInClass = " + sclass + "= " + numOfDocsInClass);
+        for (sClass sclass : sClass.values()) {
 
-            prior[i] = ((double)numOfDocsInClass / numDocsTrainingSet) ;
-            System.out.println("prior[i]" + prior[i]);
+            int numOfDocsInClass = nbHelper.countDocsInClass(sclass);
+//            System.out.println("numOfDocsInClass = " + sclass + "= " + numOfDocsInClass);
+
+            prior[i] = ((double) numOfDocsInClass / numDocsTrainingSet);
+//            System.out.println("prior[i]" + prior[i]);
             i++;
             // the total number of tokens in that class
             int textc = nbHelper.concatenateTextOfALLDocsInClass(sclass);
-            System.out.println("textc=" + textc);
+//            System.out.println("textc=" + textc);
             // the constant B ; the number of terms in the vocabulary
             int noTermsInVocab = vocabulary.size();
-            System.out.println("noTermsInVocab=" + noTermsInVocab);
-            for(Map.Entry<String,Map<sClass,Integer>> iterator : vocabulary.entrySet())
-            {
-                Map<sClass,Integer> mapClass = iterator.getValue();
-                              
-                if(null == mapClass.get(sclass))
-                {
+//            System.out.println("noTermsInVocab=" + noTermsInVocab);
+            for (Map.Entry<String, Map<sClass, Integer>> iterator : vocabulary.entrySet()) {
+                Map<sClass, Integer> mapClass = iterator.getValue();
+
+                if (null == mapClass.get(sclass)) {
                     // this word {iterator.getKey()} doesnt exist in a document for the current class {sclass}
                     //Logger.getLogger(NaiveBayes.class.getName()).log(Level.SEVERE,"{0} is not in any document of {1} class" , new Object[]{iterator.getKey(),sclass});
                     int tct = 0;
 //                    System.out.println("for " + iterator.getKey() + "tct=" + tct);
-                    double conditionalProbability = ((double)(tct + 1))/ (textc + noTermsInVocab);
+                    double conditionalProbability = ((double) (tct + 1)) / (textc + noTermsInVocab);
 //                    System.out.println("connditionalprob=" + conditionalProbability);
-                conditionalProbabilityMap.put(iterator.getKey(), conditionalProbability);
-                }
-                else
-                {
-                    // tct is the number of occurences of a term t in
+                    conditionalProbabilityMap.put(iterator.getKey(), conditionalProbability);
+                } else {
+                    // tct is the number of occurences of a term t 
                     // in training documents from class c
-                int tct = mapClass.get(sclass);
+                    int tct = mapClass.get(sclass);
 //                    System.out.println("for " + iterator.getKey() + "tct=" + tct);
-                double conditionalProbability = ((double)(tct + 1))/ (textc + noTermsInVocab);
+                    double conditionalProbability = ((double) (tct + 1)) / (textc + noTermsInVocab);
 //                    System.out.println("connditionalprob=" + conditionalProbability);
-                conditionalProbabilityMap.put(iterator.getKey(), conditionalProbability);
+                    conditionalProbabilityMap.put(iterator.getKey(), conditionalProbability);
                 }
             }
 
@@ -118,57 +115,54 @@ public class NaiveBayes {
 
         File directory = new File(SpamBundle.class.getResource(SpamBundle.DOC_CORPUS_DIR).getFile() + File.separator + testDir);
         File[] files = directory.listFiles();
-        for(File tFile: files)
-        {
-            if(!tFile.isHidden())
-            {
+        for (File tFile : files) {
+            if (!tFile.isHidden()) {
 
 //            System.out.println("working on testfile=" + tFile.getName());
-        List<String> tokens = nbHelper.extractTokensFromDoc(tFile);
+                List<String> tokens = nbHelper.extractTokensFromDoc(tFile);
 //        System.out.println("tokensize" + tokens.size());
-        double score[] = new double[sClass.values().length];
-        int i =0;
-        for(sClass sclass : sClass.values())
-        {
-            
-            score[i] = Math.log(prior[i]);
+                double score[] = new double[sClass.values().length];
+                int i = 0;
+                for (sClass sclass : sClass.values()) {
+
+                    score[i] = Math.log(prior[i]);
 //            score[i] =prior[i];
 
-            for(String token : tokens)
-            {
+                    for (String token : tokens) {
 //                System.out.println("for " + token);
-                score[i] += Math.log(conditionalProbabilityMap.get(token));
+                        score[i] += Math.log(conditionalProbabilityMap.get(token));
 //                score[i] *= conditionalProbabilityMap.get(token);
-            }
-            i++;
-        }
+                    }
+                    i++;
+                }
 //        System.out.println("spam " + score[0] + " notspam " + score[1]);
-        if(score[0] > score[1] )
-            System.out.println( tFile.getName() + " is SPAM");
-        else
-            System.out.println(tFile.getName() + " is NOTSPAM");
+                if (score[0] > score[1]) {
+                    System.out.println(tFile.getName() + " is SPAM");
+                } else {
+                    System.out.println(tFile.getName() + " is NOTSPAM");
+                }
 
+            }
         }
-        }
 
 
-        
+
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         NaiveBayes nb = new NaiveBayes();
 
         File directory = new File(SpamBundle.class.getResource(SpamBundle.DOC_CORPUS_DIR).getFile());
         File[] files = directory.listFiles();
-        for(File tFile: files)
-        {
-            if(!tFile.isHidden())
-            {
-        nb.trainMultinomialNB(tFile.getName());
-        nb.applyMultinomialNB(tFile.getName());
+        for (File tFile : files) {
+            if (!tFile.isHidden()) {
+
+                // for every run the file passed as parameter is the testing set and
+                // all other directories are the training set
+
+                nb.trainMultinomialNB(tFile.getName());
+                nb.applyMultinomialNB(tFile.getName());
             }
         }
     }
-
 }
